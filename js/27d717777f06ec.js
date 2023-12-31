15c00fb41def0d0b1b6dd49846cc1b912432ac81
bintangtenggara <51829646+bintangtenggara@users.noreply.github.com>
2023-12-31 18:57:07 +0700

    Create 27d717777f06ec.js

---
+++ js/27d717777f06ec.js
@@ -0,0 +1,29 @@
+// =============================================== //
+// - Remove any active classes (yellow boxes)
+// - please add your options (host, port, version, etc..)
+// - Destroy any pervious Kast
+// - On example box click, call Kast
+// =============================================== //
+
+    $.kast({
+        host: 'live.sg.radiobintangtenggara.com',
+        port: 9560,
+        protocol: 'http',
+        version: 1,
+        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
+        defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
+        statusBar: false, 
+        minimizeMaximize: true,
+        startTemplate: 'maximized',
+        language: {
+            offlineText: 'Temporarily Offline',
+            playedText: '<font face="Georgia" color="blue">Wes Mari Diputer :</font>',
+            unknownTrackText: 'Informasi & Solusi - c^o^d^e^l^i^s^t^.^c^c', 
+            unknownArtistText: 'Radio Bintang Tenggara' 
+        },
+        position: 'left',
+        colors: 'dynamic',
+        theme: 'dark',
+        autoPlay: true,
+        played: true
+      })
