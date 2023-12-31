a540260690cd2bb2fbf3969c1e60552508f0525c
bintangtenggara <51829646+bintangtenggara@users.noreply.github.com>
2023-12-31 19:31:20 +0700

    Update 27d717777f06ec.js

--- js/27d717777f06ec.js
+++ js/27d717777f06ec.js
@@ -13,7 +13,7 @@
         sid: 1,  
         direct: true,  
         betaProxies: false,
-        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
+        //directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: true, 
         minimizeMaximize: true,
