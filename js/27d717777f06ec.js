e525fec9c3fa86b7eadcf4f8756caf755a9fce7f
bintangtenggara <51829646+bintangtenggara@users.noreply.github.com>
2023-12-31 19:08:30 +0700

    Update 27d717777f06ec.js

--- js/27d717777f06ec.js
+++ js/27d717777f06ec.js
@@ -10,7 +10,8 @@
         port: 9560,
         protocol: 'https',
         version: 2,  
-        sid: 1,
+        sid: 1,  
+        direct: true,
         directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false, 
@@ -23,7 +24,8 @@
             unknownArtistText: 'Radio Bintang Tenggara' 
         },
         position: 'right',
-        colors: 'dynamic',  continuous: true,
+        colors: 'dynamic',  
+        continuous: true,
         theme: 'dynamic',
         autoPlay: true,
         played: true
